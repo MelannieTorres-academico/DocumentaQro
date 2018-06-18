657f491c90641fc2504cb515be7d9d1a28bcaf86
EdwardGv144 <edward.alan.gv144@gmailcom>
2018-06-17 22:09:51 -0500

    proximos_eventos corregidos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,11 +25,8 @@
 </div>
 </div>
 
-<div class="row">
+<div style="display:block;margin:0px 12px;text-align:center;" class="row"> <!--AGREGAR STYLE A CSS-->
   @include('evento.next')
 </div>
- <br><br><br><br>
- <br><br><br>
-
 
 @include('layouts.footerUser')
