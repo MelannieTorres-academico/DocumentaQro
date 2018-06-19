679f35be3a3101bfee6bdd1d82307f5313fa9bc8
EdwardGv144 <edward.alan.gv144@gmailcom>
2018-06-18 19:58:35 -0500

    footer sin subrrayado

--- resources/views/layouts/footerRedesSociales.blade.php
+++ resources/views/layouts/footerRedesSociales.blade.php
@@ -1,5 +1,15 @@
 
-<p align='center'><a href="https://www.facebook.com/DOQUMENTAmx/?hc_ref=ARSNcvXxpOsY9afxHzBls4KgTOp3yhciY73d1eyzfAdDMbaFWATGOPfY7D8jimI2abw&fref=nf" id='facebook' ><img src= "../uploads/fb.png" height='76' width='76'></a>
-<a href="https://www.instagram.com/doqumentamx/" id='instagram' ><img src= "../uploads/in.png" height='76'>
-<a href="https://twitter.com/DOQUMENTAmx" id='twitter' ><img src= "../uploads/tw.png" height='76'>
-<a href="https://www.youtube.com/documentaqro" id='youtube' ><img src= "../uploads/yt.png" height='76'></p>
+<p align='center'>
+    <a href="https://www.facebook.com/DOQUMENTAmx/?hc_ref=ARSNcvXxpOsY9afxHzBls4KgTOp3yhciY73d1eyzfAdDMbaFWATGOPfY7D8jimI2abw&fref=nf" style="text-decoration:none;" id='facebook' >
+        <img src= "../uploads/fb.png" height='76' width='76'>
+    </a>
+    <a href="https://www.instagram.com/doqumentamx/" style="text-decoration:none;" id='instagram' >
+        <img src= "../uploads/in.png" height='76'>
+    </a>
+    <a href="https://twitter.com/DOQUMENTAmx" style="text-decoration:none;" id='twitter' >
+        <img src= "../uploads/tw.png" height='76'>
+    </a>
+    <a href="https://www.youtube.com/documentaqro" style="text-decoration:none;" id='youtube' >
+        <img src= "../uploads/yt.png" height='76'>
+    </a>
+</p>
