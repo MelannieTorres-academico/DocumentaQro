e753b20679c1a53b3fb4362173e7a1ea2f0814f0
EdwardGv144 <edward.alan.gv144@gmailcom>
2018-06-18 19:03:45 -0500

    margen patrocinadores agregado

--- resources/views/patrocinadores/show.blade.php
+++ resources/views/patrocinadores/show.blade.php
@@ -1,57 +1,54 @@
 @include('layouts.head')
-
-<div class="page-header">
-  <h1><b>Nuestros orgullosos patrocinadores</b> </h1>
-</div>
-
-<div class="container">
-<h2>PRESENTADO POR:</h2>
-  <div class="row">
-    @foreach($patrocinadores as $patrocinador)
-      @if ($patrocinador -> categoria === 1)
-        <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
-          <div class="">
-            <a href="{{$patrocinador -> link}}"><img style="width:200px; max-width:200px;" src="{{ asset($patrocinador -> poster) }}" ></a>
-          </div>
-        </div>
-        @endif
-      @endforeach
+<div style="margin-bottom:20px;">
+    <div class="page-header" style="margin:30px 50px">
+      <h1><b>Nuestros orgullosos patrocinadores</b> </h1>
     </div>
 
-<hr>
-<div class="container">
-<h2>CON EL APOYO DE:</h2>
-
-      <div class="row">
-    @foreach($patrocinadores as $patrocinador)
-
-      @if ($patrocinador -> categoria === 2)
-    <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2">
-            <div class="">
-              <a href="{{$patrocinador -> link}}">  <img style="width:150px; max-width:150px;" src="{{ asset($patrocinador -> poster) }}" > </a>
-
+<div style="margin:0px 90px;">
+        <h2 style="margin:30px 0px;">PRESENTADO POR:</h2>
+          <div class="row">
+            @foreach($patrocinadores as $patrocinador)
+              @if ($patrocinador -> categoria === 1)
+                <div class="col-xs-6 col-sm-6 col-md-3 col-lg-3">
+                  <div class="">
+                    <a href="{{$patrocinador -> link}}"><img style="width:200px; max-width:200px;" src="{{ asset($patrocinador -> poster) }}" ></a>
+                  </div>
+                </div>
+                @endif
+              @endforeach
             </div>
+
+            <h2 style="margin:30px 0px;">CON EL APOYO DE:</h2>
+            <div class="row">
+                @foreach($patrocinadores as $patrocinador)
+
+                  @if ($patrocinador -> categoria === 2)
+                <div class="col-xs-4 col-sm-4 col-md-3 col-lg-2">
+                        <div class="">
+                          <a href="{{$patrocinador -> link}}">  <img style="width:150px; max-width:150px;" src="{{ asset($patrocinador -> poster) }}" > </a>
+
+                        </div>
+                    </div>
+                    @endif
+                  @endforeach
+            </div>
+
+                  <h2 style="margin:30px 0px;">EN COLABORACI&Oacute;N CON:</h2>
+                    <div class="row">
+                  @foreach($patrocinadores as $patrocinador)
+
+                    @if ($patrocinador -> categoria === 3)
+                  <div class="col-xs-3 col-sm-3 col-md-2 col-lg-2">
+                          <div class="">
+                              <a href="{{$patrocinador -> link}}"><img style="width:100px; max-width:100px;" src="{{ asset($patrocinador -> poster) }}" ></a>
+
+                          </div>
+                      </div>
+                      @endif
+                    @endforeach
+                  </div>
         </div>
-        @endif
-      @endforeach
-</div>
-
-  <hr>
-      <h1>EN COLABORACI&Oacute;N CON:</h1>
-        <div class="row">
-      @foreach($patrocinadores as $patrocinador)
-
-        @if ($patrocinador -> categoria === 3)
-      <div class="col-xs-3 col-sm-3 col-md-2 col-lg-2">
-              <div class="">
-                  <a href="{{$patrocinador -> link}}"><img style="width:100px; max-width:100px;" src="{{ asset($patrocinador -> poster) }}" ></a>
-
-              </div>
-          </div>
-          @endif
-        @endforeach
-      </div>
-</div>
+    </div>
 </div>
 
 @include('layouts.footerUser')
